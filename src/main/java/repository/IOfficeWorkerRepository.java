0c479361bdd9c8871725f0d74b48d1e5038bdbd3
Lucian Popescu <93250799+lucianpopescu688@users.noreply.github.com>
2025-05-20 00:13:01 +0300

    Delete src/main directory

--- src/main/java/repository/IOfficeWorkerRepository.java
+++
@@ -1,8 +0,0 @@
-package repository;
-import model.OfficeWorker;
-
-public interface IOfficeWorkerRepository
-{
-    void addOfficeWorker(OfficeWorker officeWorker);
-    OfficeWorker authenticate(String username, String password);
-}
